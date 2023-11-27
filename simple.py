fcb08859f3019b3a8f819328bb1388b4f76543ee
Chris Tralie <chris.tralie@gmail.com>
2023-11-26 22:37:53 -0500

    Changing vertices to letters to avoid confusion

--- simple.py
+++ simple.py
@@ -3,14 +3,14 @@ from graph import Graph
 if __name__ == '__main__':
     G = Graph()
     for i in range(6):
-        G.add_vertex(i)
-    G.add_edge(0, 1, 1)
-    G.add_edge(1, 2, 2)
-    G.add_edge(0, 2, 4)
-    G.add_edge(0, 5, 2)
-    G.add_edge(4, 5, 4)
-    G.add_edge(2, 4, 5)
-    G.add_edge(3, 4, 2)
-    G.add_edge(2, 3, 10)
+        G.add_vertex(chr(ord("a")+i))
+    G.add_edge("a", "b", 1)
+    G.add_edge("b", "c", 2)
+    G.add_edge("a", "c", 4)
+    G.add_edge("a", "f", 2)
+    G.add_edge("e", "f", 4)
+    G.add_edge("c", "e", 5)
+    G.add_edge("d", "e", 2)
+    G.add_edge("c", "d", 10)
 
-    
+    print(G.explore("a"))
